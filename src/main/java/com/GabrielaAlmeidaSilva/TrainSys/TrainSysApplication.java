3e3d3004d39878d1cfcb41eb54ec0303599d974d
gabiAlmeidaDev <153233088+gabiAlmeidaDev@users.noreply.github.com>
2024-06-17 20:25:01 -0300

    first commit

---
+++ src/main/java/com/GabrielaAlmeidaSilva/TrainSys/TrainSysApplication.java
@@ -0,0 +1,13 @@
+package com.GabrielaAlmeidaSilva.TrainSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GabrielaAlmeidaSilva/TrainSys/TrainSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GabrielaAlmeidaSilva.TrainSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
