ecab1d033a1b6fdc79d3a2abb6cae06e2cfd4417
gabiAlmeidaDev <153233088+gabiAlmeidaDev@users.noreply.github.com>
2024-06-18 19:31:29 -0300

    PlanEntity e Plan Repository

---
+++ src/main/java/com/GabrielaAlmeidaSilva/TrainSys/Repository/PlanRepository.java
@@ -0,0 +1,9 @@
+package com.GabrielaAlmeidaSilva.TrainSys.Repository;
+
+
+import com.GabrielaAlmeidaSilva.TrainSys.Entities.Plan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanRepository extends JpaRepository<Plan, Long> {
+}
+
