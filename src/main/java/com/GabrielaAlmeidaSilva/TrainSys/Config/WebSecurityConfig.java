33cb6256ca54d5d2e78c2d15f2636244954535ff
gabiAlmeidaDev <153233088+gabiAlmeidaDev@users.noreply.github.com>
2024-06-22 16:52:46 -0300

    Configuração de segurança para acesso total ao 'post' do usuario

---
+++ src/main/java/com/GabrielaAlmeidaSilva/TrainSys/Config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.GabrielaAlmeidaSilva.TrainSys.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/api/users").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(httpBasic -> {});
+
+        return http.build();
+    }
+}
